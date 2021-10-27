8ee8065354b88009a8547feb1f7ff8ebd86196de
Orsimer <jakonhovland96@gmail.com>
2021-10-27 19:17:03 +0300

    AGAIN improved IRP1/task2

--- univ/Labs/IRP1/task2/task2.cpp
+++ univ/Labs/IRP1/task2/task2.cpp
@@ -6,23 +6,23 @@
 int main() {
     using namespace std;
     setlocale(LC_CTYPE, "UA");
-    
-    long double integral = 0, a, b;
-    long double x1, y1, x2, y2;
 
-    //open input file
+
+    long double a, b, integral = 0;
+    long double x1, x2, y1, y2;
+
     ifstream input("input.in");
-    //initial reading is needed to define a
-    input >> x1 >> y1 >> x2 >> y2;
-    a = x1;
-    
-    //calculating integral
-    do {
-        integral += (x2-x1)*(y1+y2)/2;
+    if (!input) cerr << "Файлу не існує!"; //foolproof
+    input >> x1 >> y1;
+    a = x1; //the starting point of the integral
 
-        x1 = x2; y1 = y2;
+    //calculation of the integral
+    do {
+
         input >> x2 >> y2;
-        b = x2;
+        integral += (x2-x1)*(y1+y2)/2;
+        x1 = x2; y1 = y2;
+        b = x1; //the ending point of the integral
 
     } while (!input.eof());
 
@@ -34,8 +34,11 @@ int main() {
     output.setf(ios::fixed);
     output.precision(8);
 
-    cout << "Інтервал інтегрування: ["  << a << "; " << b << "]\n";
+    cout << "Інтервал інтегрування: ["  << a  << "; " << b << "]\n";
     cout << "Значення інтегралу: " << integral;
-    output << "Інтервал інтегрування: [" << a  << "; " << b << "]\n";
+    output << "Інтервал інтегрування: ["  << a  << "; " << b << "]\n";
     output << "Значення інтегралу: " << integral;
+
+    input.close(); output.close();
 }
+
