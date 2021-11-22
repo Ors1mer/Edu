000c2cdd8eaeeccb5624b366dba82626f9a833fe
Orsimer <jakonhovland96@gmail.com>
2021-11-22 16:22:51 +0200

    Fully finished IRP2 with the only source file task.cpp

--- univ/Labs/IRP2/task.cpp
+++ univ/Labs/IRP2/task.cpp
@@ -1,32 +1,74 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 const double eps = 1e-3;
+
+//iterable solution
 
 long double iter_cos(long double x) {
     long double sum = 1, element = 1;
     int16_t sign = -1;
-    uint32_t n = 1;
+    uint32_t n = 2;
     while (fabs(element) > eps) {
-        element *= sign*(x*x)/(2*n*(2*n - 1));
-        std::cout << element << ' ' << 2*n*(2*n-1) << ' ' <<sign*x*x << '\n';
-        sum += element;
+        element *= x*x/(n*(n - 1));
+        sum += sign*element;
+        
         sign = -sign;
-        ++n;
+        n += 2;
     }
     return sum;
-
 }
+
+//recursive solution
+
+long double Sum(long double a, long double b, long double x) {
+    if (fabs(a/b) > eps) {
+        return a/b + Sum(a*-x*x, b*(b+1)*(b+2), x);
+    } else {
+        return 0;
+    }
+}
+
+long double recur_cos(long double x) {
+    return 1 + Sum(-x*x, 2, x);
+}
+
+//tabulation
+
+void full_tabula(long double a, long double b, int n) {
+    using namespace std;
+    long double dx = (b-a)/n;
+    long double x = a + dx;
+
+    cout << 'x' <<"\t\t"<< "iter_cos(x)" <<'\t'<< "recur_cos(x)" <<'\t'<< "cos(x)" <<'\n';
+    cout << fixed << setprecision(6);
+    while (x < b) {
+        cout << x <<'\t'<< iter_cos(x) <<'\t'<< recur_cos(x) <<'\t'<< cos(x) <<'\n';
+        x += dx;
+    }
+}
+
+void tabula(long double a, long double b, int n, long double (*func)(long double)) {
+    using namespace std;
+    long double dx = (b-a)/n;
+    long double x = a + dx;
+
+    cout << 'x' <<"\t\t"<< "func(x)\n";
+    cout << fixed << setprecision(6);
+    while (x < b) {
+        cout << x <<'\t'<< func(x) << '\n';
+        x += dx;
+    }
+}
+
+
 
 int main() {
     using namespace std;
     setlocale(LC_CTYPE, "UA");
+    
+    tabula(-1, 1, 20, cos);
 
-    long double x;
-    cout << "Введiть значення аргументу: ";
-    cin >> x;
-
-    cout << iter_cos(x) << '\n';
-
-
+    return 0;
 }
