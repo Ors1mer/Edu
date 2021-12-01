6b34166c16abaaa31ee685bf3cbccb2eae031eaa
Ors1mer <89401578+Ors1mer@users.noreply.github.com>
2021-12-01 21:16:30 +0200

    Last commit on Lab5
    Rewritten Lab5/task5

--- univ/Labs/Lab5/task5/task.cpp
+++ univ/Labs/Lab5/task5/task.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-
-int64_t max(const int64_t &a, const int64_t &b) {
+/*
+short max(const char& a, const char& b) {
     return a > b ? a : b;
 }
 
-long double max(const long double &a, const long double &b) {
+long max(const long& a, const long& b) {
+    return a > b ? a : b;
+}
+
+double max(const double& a, const double& b) {
+    return a > b ? a : b;
+}
+*/
+short max(const short& a, short b = 1) {
     return a > b ? a : b;
 }
 
 template <typename num>
-num max(const num  &a, const num &b) {
+num max(const num& a, const num& b) {
     return a > b ? a : b;
 }
 
@@ -17,9 +25,5 @@ int main() {
     using std::cout, std::cin;
     setlocale(LC_CTYPE, "UA");
 
-    long double a, b;
-    cout << "Введiть два числа: ";
-    cin >> a >> b;
-
-    cout << max(a, b);
+    cout << max(12, 6.28) << '\n';
 }
