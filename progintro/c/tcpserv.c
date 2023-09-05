ddabceb8e1a4abc433956d9b1b686bcb5f3d62f1
Ors1mer <ors1mer_dev [[at]] proton.me>
2023-09-05 15:34:28 +0100

    rewrite simple tcp server

--- progintro/c/tcpserv.c
+++ progintro/c/tcpserv.c
@@ -12,10 +12,11 @@
 #include <errno.h>
 #include <stdio.h>
 
-#define UDP_PACKET_SIZE 508
 #define NOFLAG 0
+#define QLEN 128
+#define PACKET_SIZE 150
 
-#define DEMONIZE() \
+#define DAEMONIZE() \
     close(0); \
     close(1); \
     close(2); \
@@ -31,11 +32,11 @@
     if(pid > 0) \
         exit(0)
 
-#define HANDLE_ERR_CLOSE(errcode, msg, sockd, errexit) \
+#define HANDLE_ERR_CLOSE(errcode, sockd, msg, errexit) \
     if(errcode == -1) { \
         perror(msg); \
         close(sockd); \
-        return errcode; \
+        return errexit; \
     }
 
 enum {
@@ -63,90 +64,105 @@ void strncopy(char *dest, const char *src, int n)
         *dest = *src;
 }
 
-
-int bind_socket(int sockfd, int ip, int port)
-{
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(ip);
-    return bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-}
-
-
-char *write_date(char *dest)
+char *write_date(char *dest, unsigned int size)
 {
     size_t len;
     time_t current;
     current = time(NULL);
-    len = strftime(dest, UDP_PACKET_SIZE, "Timestamp: %x %I:%M %p\n",
+    len = strftime(dest, size, "Timestamp: %x %I:%M %p\n",
                    localtime(&current));
     /* return pointer to the end of string */
     return dest + len;
 }
 
-char *fill_buff(const struct sockaddr_in *addr, size_t *buffsize)
+char *fill_buffer(const struct sockaddr_in *addr, size_t *buffsize)
 {
     char *buff, *curr_p;
     char ipstr[16];
     int len;
-    buff = calloc(sizeof(char), UDP_PACKET_SIZE);
+    unsigned short port;
+    buff = calloc(sizeof(char), PACKET_SIZE);
 
-    curr_p = write_date(buff);
+    curr_p = write_date(buff, PACKET_SIZE);
 
+    port = ntohs(addr->sin_port);
     strncopy(ipstr, inet_ntoa(addr->sin_addr), sizeof(ipstr));
-    len = snprintf(curr_p, UDP_PACKET_SIZE-(curr_p-buff), "IP: %s\n", ipstr);
+
+    len = snprintf(curr_p, PACKET_SIZE-(curr_p-buff), "IP: %s\nPort: %d\n",
+                   ipstr, port);
     curr_p += len;
 
     *buffsize = curr_p - buff;
     return buff;
 }
 
-void answer_clients(int sockd)
+int bind_socket(int sockd, unsigned int port)
 {
-    struct sockaddr_in client;
-    socklen_t addrsize = sizeof(client);
-    int sockd_client, errcode;
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;    
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    return bind(sockd, (struct sockaddr*)&addr, sizeof(addr));
+}
+
+unsigned short get_port(const char *strport)
+{
+    unsigned short port;
+    port = atoi(strport);
+    if(port == 0) {
+        fputs("The port must be a number between 1024 and 65535!\n", stderr);
+    } else if(port < 1024) {
+        fputs("A privileged port specified (port < 1024)!\n", stderr);
+        return 0;
+    }
+    return port;    
+}
+
+void answer_clients(int server_sockd)
+{
+    int client_sockd;
+    struct sockaddr_in addr;
+    socklen_t addrlen = sizeof(addr);
     for(;;) {
+        size_t buffsize;
         char *buff;
-        size_t size;
-        sockd_client = accept(sockd, (struct sockaddr*)&client, &addrsize);
+        client_sockd = accept(server_sockd, (struct sockaddr*)&addr, &addrlen);
+        
+        buff = fill_buffer(&addr, &buffsize);
 
-        buff = fill_buff(&client, &size);
+        write(client_sockd, buff, buffsize);
 
-        errcode = sendto(sockd_client, buff, size, NOFLAG,
-                         (const struct sockaddr*)&client, addrsize);
-        if(errcode == -1)
-            perror("sendto");
         free(buff);
-        shutdown(sockd_client, SHUT_RDWR);
-        close(sockd_client);
+        shutdown(client_sockd, SHUT_RDWR);
+        close(client_sockd);
     }
 }
 
-int main(int argc, const char **argv)
+int main(const int argc, const char **argv)
 {
-    int pid, sockd, errcode, opt = 1;
-    #ifndef DEBUG
-    DEMONIZE();
-    #endif
+    int sockd, errcode, option, pid;
+    unsigned short port;
     if(argc < 2) {
-        fprintf(stderr, "No port specified!\n");
+        fputs("Not enough arguments: no port specified!\n", stderr);
         return argerr;
     }
+    port = get_port(argv[1]);
+    if(port == 0)
+        return argerr;
     sockd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(sockd < 0) {
-        fprintf(stderr, "Couldn't create the socket!\n");
+    if(sockd == -1) {
+        perror("Socket error");
         return sockerr;
     }
-    setsockopt(sockd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    errcode = bind_socket(sockd, INADDR_ANY, atoi(argv[1]));
-    HANDLE_ERR_CLOSE(errcode, "bind", sockd, binderr);
-
-    errcode = listen(sockd, 128);
-    HANDLE_ERR_CLOSE(errcode, "listen", sockd, listenerr);
-
+    /* solve sticky tcp port problem */
+    setsockopt(sockd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+    errcode = bind_socket(sockd, port);
+    HANDLE_ERR_CLOSE(errcode, sockd, "Error binding socket", binderr)
+    errcode = listen(sockd, QLEN);
+    HANDLE_ERR_CLOSE(errcode, sockd, "Error setting listen state", sockerr);
+    DAEMONIZE();
     answer_clients(sockd);
-
+    close(sockd);
     return 0;
 }
+
